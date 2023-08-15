94006660fc331268e9976ce8a2ccb543097dc70b
miroman9364 <107484955+miroman9364@users.noreply.github.com>
2023-08-15 06:49:57 -0700

    Update tuples topic

--- src/tuples.rs
+++ src/tuples.rs
@@ -1,4 +1,16 @@
 /* https://youtu.be/zF34dRivLOw?t=2683
+
+A `tuple` is:
+
+- a group of elements
+- max of 12 elements
+- can be different types
+
+TODO: Find out if a tuple is a native type, standard library type, or some kind of syntactic sugar?
 */
 
-pub fn run() {}
+pub fn run() {
+    let person: (&str, &str, i8) = ("Mike", "Seattle", 54);
+
+    println!("{} is from {} and is {}.", person.0, person.1, person.2);
+}
