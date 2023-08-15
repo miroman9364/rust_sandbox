0e49d86f475745d5c2432e4e367599d8aee5ff67
miroman9364 <107484955+miroman9364@users.noreply.github.com>
2023-08-15 13:57:05 -0700

    Update conditionals topic

--- src/conditionals.rs
+++ src/conditionals.rs
@@ -1,4 +1,16 @@
 /* https://youtu.be/zF34dRivLOw?t=3615
 */
 
-pub fn run() {}
+pub fn run() {
+    let age: u8 = 18;
+    let check_id: bool = false;
+    let knows_person_is_old_enough = true;
+
+    if age >= 21 && check_id || knows_person_is_old_enough{
+        println!("what do you want to drink?");
+    } else if age < 21 && check_id {
+        println!("get outta here before I call the cops you mug!");
+    } else {
+        println!("show me your ID");
+    }
+}
