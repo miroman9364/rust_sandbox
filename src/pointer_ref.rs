bffe178eb68325f8735dd1222144fceeefe411f6
miroman9364 <107484955+miroman9364@users.noreply.github.com>
2023-08-16 08:43:29 -0700

    Update pointer_ref topic

--- src/pointer_ref.rs
+++ src/pointer_ref.rs
@@ -1,4 +1,16 @@
 /* https://youtu.be/zF34dRivLOw?t=4740
 */
 
-pub fn run() {}
+pub fn run() {
+    // access primitive
+    let arr1: [i32; 3] = [1, 2, 3];
+    let arr2: [i32; 3] = arr1;
+
+    println!("values: {:?}", (arr1, arr2));
+
+    // access user types
+    let vec1: Vec<i32> = vec![1, 2, 3];
+    let vec2: &Vec<i32> = &vec1;
+
+    println!("values: {:?}", (&vec1, vec2));
+}
